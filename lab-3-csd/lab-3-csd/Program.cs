6f4440c84720f26b3479d3038315c6aa050f7fb4
Niklas Appelquist <niklasappelquist@gmail.com>
2021-02-15 12:10:29 +0100

    first commit

---
+++ lab-3-csd/lab-3-csd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_3_csd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
