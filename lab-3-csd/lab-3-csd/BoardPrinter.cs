eb55de1d65deb774858212dbf15f6c79e6a45a05
Niklas Appelquist <niklasappelquist@gmail.com>
2021-03-05 10:56:01 +0100

    Added BoardPrinter class

---
+++ lab-3-csd/lab-3-csd/BoardPrinter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3_csd
+{
+    class BoardPrinter
+    {
+        private List<string> result = new List<string>();
+        public BoardPrinter()
+        {
+        }
+        public void PrintBoard(IBoard board)
+        {
+            List<string> winningCells = board.GetWinningCells();
+            for (int i = 0; i < winningCells.Count; i++)
+            {
+                foreach (IBoard b in board.GetCells())
+                {
+                    if (b.GetCoordinate() == winningCells[i])
+                    {
+                        for (int j = 0; j < b.GetWinningCells().Count; j++)
+                        {
+                            result.Add(winningCells[i] + "." + b.GetWinningCells()[j]);
+                        }
+                        PrintBoard(b);
+                    }
+                }
+            }
+            return;
+        }
+    }
+}
